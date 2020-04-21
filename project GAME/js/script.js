00a4042a4ce1244d81271aa6ce76fb42fb0b34a3
Huga55 <58032442+Huga55@users.noreply.github.com>
2020-04-21 19:51:44 +0500

    Final project game

--- project GAME/js/script.js
+++ project GAME/js/script.js
@@ -2,6 +2,23 @@ $('.burger').click(function() {
   $('.nav').toggleClass('nav_visible');
 });
 
+$('.header__button-help').click(function() {
+  $('.modal').toggleClass('modal_active');
+  $('.modal__window').toggleClass('modal__window_active');
+});
+
+$('.modal__close').click(function() {
+  $('.modal').toggleClass('modal_active');
+  $('.modal__window').toggleClass('modal__window_active');
+})
+
+let gamesWindow = $('.slider-games__status');
+
+for(let game of gamesWindow) {
+  if(game.innerHTML.indexOf('Detected') != -1) {
+    game.disabled = true;
+  }
+}
 
 
 $(document).ready(function(){
@@ -12,5 +29,56 @@ $(document).ready(function(){
       dots: true,
       dotsClass: 'header__switches',
       arrows:false,
+  });
+});
+
+
+$(document).ready(function(){
+  $('.slider-games').slick({
+      infinite: true,
+      centerMode: true,
+      centerPadding: '110px',
+      slidesToShow: 5,
+      prevArrow: '.roll-games__left',
+      nextArrow: '.roll-games__right',
+      autoplay: true,
+      autoplaySpeed: 2500,
+         responsive: [
+      {
+        breakpoint: 1078,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 956,
+        settings: {
+          centerPadding: '100px',
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 794,
+        settings: {
+          centerMode: false,
+          centerPadding: '0px',
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 565,
+        settings: {
+          centerPadding: '0px',
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 408,
+        settings: {
+          centerPadding: '0px',
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 });
